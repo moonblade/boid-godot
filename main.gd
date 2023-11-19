d09bb02712294e002090ca9187aa2c62f79c17ed
mnishamk <mnishamk@commvault.com>
2023-11-19 12:30:02 +0530

    vars

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var boid = load("res://boid.tscn")
-	for x in range(100):
+	for x in range(50):
 		var instance = boid.instantiate()
 		add_child(instance)
 	pass # Replace with function body.
