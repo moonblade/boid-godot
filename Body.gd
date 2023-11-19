2a6563e0d5c7117ff04e09b9afcbe67df08f4776
mnishamk <mnishamk@commvault.com>
2023-11-19 11:45:42 +0530

    Boids work

--- Body.gd
+++ Body.gd
@@ -1,11 +1,20 @@
 extends RigidBody2D
-@export var speed = 400
+@export var maxspeed = 1000
+@export var minspeed = 200
 @export var angular_speed = PI
+@export var avoidFactor = 0.5
+@export var matchFactor = 0.5
+@export var centeringFactor = 0.05
+@export var turnFactor = 10
 var target
+var avoidList = []
+var alignList = []
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	target = getRandomPoint()
+	self.position = Vector2(randi()%600 + 200,randi()%600 + 200)
+	self.linear_velocity = Vector2(randi()%200, randi()%200)
 	pass # Replace with function body.
 
 func getRandomPoint():
@@ -30,10 +39,61 @@ func checkHit():
 		target = getRandomPoint()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var velocity = Vector2.UP.rotated(self.rotation) * speed
-	position += velocity * delta
-#	var target = $"../Target".position
-	var direction = getRotationDirection(position, position + Vector2.UP.rotated(self.rotation) * 200, target)
-	self.rotation += angular_speed * delta * direction
-	checkHit()
+func _process(delta):	
+	# Seperation
+	var close = Vector2(0,0)
+	for boid in avoidList:
+		close += self.position - boid.position
+	self.linear_velocity += close * avoidFactor
+	
+	# Alignment
+	var avgVelocity = Vector2(0,0)
+	for boid in alignList:
+		if boid not in avoidList:
+			avgVelocity += boid.linear_velocity
+	avgVelocity /= len(alignList)
+	self.linear_velocity += (avgVelocity - self.linear_velocity) * matchFactor
+
+	# Cohesion
+	var avgPosition = Vector2(0,0)
+	for boid in alignList:
+		if boid not in avoidList:
+			avgPosition += boid.position
+	avgPosition /= len(alignList)
+	self.linear_velocity += (avgPosition - self.position) * centeringFactor
+	$AnimatedSprite2D.rotation = self.linear_velocity.angle() + PI/2
+
+	#Clamp velocity
+	var velocity = linear_velocity.length()
+	if velocity < minspeed:
+		linear_velocity = linear_velocity/velocity * minspeed
+	if velocity > maxspeed:
+		linear_velocity = linear_velocity/velocity * maxspeed
+		
+	# Edge os screen
+	if position.x < 200:
+		self.linear_velocity.x += turnFactor
+	if position.x > get_viewport().size.x - 200:
+		self.linear_velocity.x -= turnFactor
+	if position.y < 200:
+		self.linear_velocity.y += turnFactor
+	if position.y > get_viewport().size.y - 200:
+		self.linear_velocity.y -= turnFactor
+	
+func _on_area_2d_body_entered(body):
+	if body != self:
+		avoidList.append(body)
+
+func _on_area_2d_body_exited(body):
+	if body != self:
+		avoidList.erase(body)
+
+
+func alignEnter(body):
+	if body != self:
+		alignList.append(body)
+
+
+func alignExit(body):
+	if body != self:
+		alignList.erase(body)
---
+++ CollisionShape2D.gd
@@ -0,0 +1,15 @@
+extends CollisionShape2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _draw():
+#	draw_circle(self.position, self.shape.radius, "ff000020")
+	pass
+	
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
